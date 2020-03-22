6c7cf5b6e3633071701212a34c2a424a8a7cc1e4
Benjamin de Boissieu <bendeb@hotmail.com>
2020-03-22 10:55:29 +0100

    with image

--- src/components/active_city.jsx
+++ src/components/active_city.jsx
@@ -6,10 +6,12 @@ class ActiveCity extends Component {
     let displayCity = null;
 
     if(this.props.selectedCity){
+      const url = `https://kitt.lewagon.com/placeholder/cities/${this.props.selectedCity.slug}`;
       displayCity =
         <div>
           <h1>{this.props.selectedCity.name}</h1>
           <p>{this.props.selectedCity.address}</p>
+          <img src={url} alt={this.props.selectedCity.name} width= "100%"/>
         </div>
     }
 
